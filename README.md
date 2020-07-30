abdfaf337e11f2120709f4c1a3d8f8b5e3298b14
Let's Write - Augustus <evilkim25@gmail.com>
2020-07-30 22:03:37 +0800

    Demo完成

---
+++ README.md
@@ -0,0 +1,5 @@
+# Google表單，輸入ID後自動帶入其它欄位資料
+
+Demo：[demo](https://letswritetw.github.io/letswrite-google-forms-id-data/)
+
+筆記文：[Let's Write](https://letswrite.tw/google-forms-id-data/)
---
+++ src/main.js
@@ -0,0 +1,80 @@
+const FormAutoFill = new Vue({
+  el: '#app',
+  data: {
+
+    // Google Apps Script 部署為網路應用程式後的 URL
+    gas: 'https://script.google.com/macros/s/AKfycbwyY7oLpoZ4lbcIwIWniLZ8U_pY8e7YBK9fakeszgR_77S2u1SV/exec',
+
+    id: '',
+
+    // 避免重複 POST，存資料用的
+    persons: {},
+
+    // 頁面上吐資料的 data
+    person: {},
+
+    // Google Form 的 action URL
+    formAction: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeNBnd-yVJ7_-tMq5xaQrvt0j18UtabCFBTM0Eu2O3ivDecuQ/formResponse',
+    
+    // Google Form 各個 input 的 name
+    input: {
+      id: 'entry.1815052017',
+      name: 'entry.1543576845',
+      gender: 'entry.892230025',
+      phone: 'entry.1003128242',
+      site: 'entry.260985931',
+      msg: 'entry.1782841550'
+    },
+
+    // loading 效果要不要顯示
+    loading: false
+  },
+  methods: {
+    // ID 限填 4 碼
+    limitIdLen(val) {
+      if(val.length > 4) {
+        return this.id =  this.id.slice(0, 4);
+      }
+    },
+    // 送出表單
+    submit() {
+      // 再一次判斷是不是可以送出資料
+      if(this.person.name !== undefined) {
+        let params = `${this.input.id}=${this.person.id}&${this.input.name}=${this.person.name}&${this.input.gender}=${this.person.gender}&${this.input.phone}=${this.person.phone}&${this.input.site}=${this.person.site}&${this.input.msg}=${this.person.message || '無'}`;
+        fetch(this.formAction + '?' + params, {
+          method: 'POST'
+        }).catch(err => {
+            alert('提交成功。');
+            this.id = '';
+            this.person = {};
+          })
+      }
+    }
+  },
+  watch: {
+    id: function(val) {
+      // ID 輸入到 4 碼就查詢資料
+      if(val.length === 4) {
+
+        // this.persons 裡沒這筆資料，才 POST
+        if(this.persons[this.id] === undefined) {
+          this.loading = true;
+          let uri = this.gas + '?id=' + this.id;
+          fetch(uri, {
+            method: 'POST'
+          }).then(res => res.json())
+            .then(res => {
+              this.persons[this.id] = res; // 把這次查詢的 id 結果存下來
+              this.person = res;
+              this.loading = false;
+            })
+        }
+        // this.persons 裡有資料就吐資料
+        else {
+          this.person = this.persons[this.id];
+        }
+
+      }
+    }
+  }
+})
